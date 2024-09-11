96440ca646607dc324ab5924690f3f73cca0254e
CoDanTheBarbarian <plafcandaniel@gmail.com>
2024-09-10 22:36:07 -0700

    13: Added a cooldown to the player class and updated the update and shoot methods to implement a timed shot limit.

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
@@ -17,7 +18,7 @@ class Player(CircleShape):
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
-        return [a, b, c]\
+        return [a, b, c]
         
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -27,6 +28,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
+        self.cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -38,9 +40,11 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(0 - dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.cooldown <= 0:
+                self.shoot()
 
     def shoot(self):
+        self.cooldown = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity *= PLAYER_SHOT_SPEED
