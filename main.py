2050af488a1e2bc8bfa02cac7a2f1cea3f00c0b6
CoDanTheBarbarian <plafcandaniel@gmail.com>
2024-09-10 21:28:17 -0700

    9: Created sprite groups for updateable sprites and drawable sprites. Added them to a static field called containers in the Player class. Changed the draw and update methods in main to draw the drawables and update the updateables. Order of operations is important here, you didn't get it right at first.

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for j in updateable:
+            j.update(dt)
         screen.fill('black')
-        player.draw(screen)
-        player.update(dt)
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
